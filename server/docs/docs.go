9efb5ff5fedef72e38673afbb13015cbef93bcf2
ggorockee <ggorockee@gmail.com>
2025-12-04 01:35:45 +0900

    chore(server): API 버전 1.0.1 업데이트

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -667,7 +667,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0.0",
+	Version:          "1.0.1",
 	Host:             "api.woohalabs.com",
 	BasePath:         "/ojeomneo/v1",
 	Schemes:          []string{"https", "http"},
